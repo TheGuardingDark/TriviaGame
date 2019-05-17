41eba6a5c691e11413ced1c3986ad5995cd9bed2
Scout <therearespidersinthisemail@gmail.com>
2019-05-17 17:04:36 -0600

    added first timer to quiz

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,12 @@ const quiz = document.getElementById('quiz');
 var answer = document.getElementById('answer');
 const results = document.getElementById('results');
 const submitBtn = document.getElementById('submit');
-const answerHolder = [];
-
+const round = document.querySelectorAll(".round");
+let roundNum = 0;
+let correct = 0;
+let wrong = 0;
+let unanswered = 0;
+var intervalId;
 
 const quizQuestions = [
     {
@@ -43,7 +47,19 @@ const quizQuestions = [
     },
 ];
 
+
+// function showRound(n) {
+//     round[roundNum].classList.remove('currentRound');
+//     round[n].classList.add('currentRound');
+//     roundNum = n;
+//     submitBtn.style.display = 'inline-block';
+// };
+
+// showRound(0);
+
 function showQuiz(){
+ time = 30;
+    intervalId = setInterval(timer, 1000);
     const output = [];
     quizQuestions.forEach(
         (currentQuestion, questionNumber) => {
@@ -60,13 +76,36 @@ function showQuiz(){
             }
 
             output.push(
-                `<div class="question"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
+                `<div class="round">
+                    <div class="question"> ${currentQuestion.question} </div>
+                        <div class="answers"> ${answers.join('')} </div>
+                </div>`
             );
         }
     );
 
     quiz.innerHTML = output.join('');
+};
+
+function timer(){
+    time--;
+    var currentTime = timeConverter(time);
+    $("#timer").text(currentTime);
+};
+
+function timeConverter(t){
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+            minutes = "00";
+        }
+        else if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        return minutes + ":" + seconds;
 };
 
 
